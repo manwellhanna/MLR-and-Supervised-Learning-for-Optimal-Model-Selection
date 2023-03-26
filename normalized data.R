345201df60b9b09eb864cf8228061e3aad1dd25f
patilanja <128924991+patilanja@users.noreply.github.com>
2023-03-26 12:10:39 -0400

    latest code including outliers replacement

--- normalized data.R
+++ normalized data.R
@@ -5,7 +5,8 @@ install.packages("readxl")
 library(readxl)
 install.packages("BBmisc")
 library(BBmisc)
-
+install.packages('Hmisc')
+library(Hmisc)
 df<- read_excel("Train.xlsx")
 df
 summary(df)
@@ -14,9 +15,21 @@ summary(df)
 boxplot(df, main = "Boxplot of My Data", xlab = "My Data", ylab = "Values")
 
 
+#replace outliers
+
+replaceOuts = function(df) {
+  map_if(df, is.numeric, 
+         ~ replace(.x, .x %in% boxplot.stats(.x)$out, median(.x))) %>%
+    bind_cols 
+}
+
+
+newdf<-replaceOuts(df[,1:11])
+boxplot(newdf, main = "Boxplot of My Data", xlab = "My Data", ylab = "Values")
+
 
 # method = range for normalisation 
-scaled_df = normalize(df[,1:12], method = "range", range = c(0, 1))
+scaled_df = normalize(newdf[,1:11], method = "range", range = c(0, 1))
 
 summary(scaled_df)
 scaled_df
@@ -25,3 +38,5 @@ scaled_df
 
 df1<-data.frame(scaled_df,Y=df$Y)
 df1
+boxplot(df1[,1:11], main = "Boxplot of My Data", xlab = "My Data", ylab = "Values")
+
